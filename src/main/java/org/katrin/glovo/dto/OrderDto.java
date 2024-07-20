239930732335af608ad890213660ce8ff2a65880
KatrinGratilova <gr.katrin.05@gmail.com>
2024-07-20 18:22:40 +0300

    Removed a modified file from pull request

--- src/main/java/org/katrin/glovo/dto/OrderDto.java
+++ src/main/java/org/katrin/glovo/dto/OrderDto.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,7 +12,6 @@ import java.util.List;
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class OrderDto {
     private int id;
 
