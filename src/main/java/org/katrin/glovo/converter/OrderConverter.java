e6e150476705bfa7c89da41af8eaccf99c31d741
KatrinGratilova <gr.katrin.05@gmail.com>
2024-07-20 17:30:24 +0300

    added converter tests

--- src/main/java/org/katrin/glovo/converter/OrderConverter.java
+++ src/main/java/org/katrin/glovo/converter/OrderConverter.java
@@ -4,7 +4,7 @@ import org.katrin.glovo.dto.OrderDto;
 import org.katrin.glovo.entity.OrderEntity;
 import org.katrin.glovo.entity.OrderItemEntity;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class OrderConverter {
     public static OrderDto toDto(OrderEntity orderEntity) {
@@ -18,12 +18,13 @@ public class OrderConverter {
     }
 
     public static OrderEntity toEntity(OrderDto orderDto) {
+        List<OrderItemEntity> orderItemEntities = orderDto.getItems().stream().map(i -> OrderItemEntity.builder().id(i).build()).toList();
         return OrderEntity.builder()
                 .id(orderDto.getId())
                 .customerName(orderDto.getCustomerName())
                 .status(orderDto.getStatus())
                 .checkoutDate(orderDto.getCheckoutDate())
-                .items(new ArrayList<>())
+                .items(orderItemEntities)
                 .build();
     }
 }
